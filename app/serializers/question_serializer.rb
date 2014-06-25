fa8da5edffe9015b2c57a1d5ff7799eef4642830
Aaron Tsang <aarontsang@Aarons-MacBook-Pro.local>
2014-06-24 17:30:45 -0700

    added timestamp for updated

--- app/serializers/question_serializer.rb
+++ app/serializers/question_serializer.rb
@@ -1,3 +1,3 @@
 class QuestionSerializer < ActiveModel::Serializer
-  attributes :id, :name, :topic, :note, :created_at, :instructor
+  attributes :id, :name, :topic, :note, :created_at, :instructor, :updated_at
 end
